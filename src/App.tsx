d09a90c85d0657c39139437a6b2bfe2c9e5aeb4c
nyoooom127 <tamar.peles@gmail.com>
2024-01-08 08:32:36 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,44 @@ import Sidebar from "./Components/LayoutArea/Sidebar/Sidebar";
 import { useSelector } from "react-redux";
 import { AppState } from "./Redux/AppState";
 import Login from "./Components/AuthArea/Login/Login";
+import ConstraintType from "./Models/ConstraintType";
+import { Client } from "@stomp/stompjs";
+import { StompSessionProvider, useSubscription } from "react-stomp-hooks";
+
+const SOCKET_URL = "http://localhost:8080/ws-message";
 
 function App() {
   const [sideBarOpen, setSideBarOpen] = React.useState(false);
   const auth = useSelector((appState: AppState) => appState.auth);
+
+  // React.useEffect(() => {
+  //   const client = new Client({
+  //     brokerURL: SOCKET_URL,
+  //     reconnectDelay: 5000,
+  //     heartbeatIncoming: 4000,
+  //     heartbeatOutgoing: 4000,
+  //     onConnect: onConnected,
+  //     onDisconnect: onDisconnected,
+  //   });
+
+    // function onConnected() {
+    //   console.log("Connected!!");
+    //   client.subscribe("/topic/message", function (msg) {
+    //     if (msg.body) {
+    //       const jsonBody = JSON.parse(msg.body);
+    //       if (jsonBody.name) {
+    //         setMessage(jsonBody.name);
+    //       }
+    //     }
+    //   });
+    // }
+
+  //   function onDisconnected() {
+  //     console.log("Disconnected!!");
+  //   }
+
+  //   client.activate();
+  // }, []);
 
   return (
     <Box sx={{ display: "flex" }} className="App">
@@ -19,15 +53,45 @@ function App() {
       {auth ? (
         <>
           <Sidebar open={sideBarOpen} setOpen={setSideBarOpen} />
+          {/* <StompSessionProvider url={SOCKET_URL}>
+            <SubscribingComponent/>
+          </StompSessionProvider> */}
           {/* <Box component="main" sx={{ flexGrow: 1, p: 3 }}> */}
-            <Content />
+          <Content />
           {/* </Box> */}
         </>
       ) : (
-        <Login/>
+        <Login />
       )}
     </Box>
   );
 }
+
+// function SubscribingComponent() {
+//   const [message, setMessage] = React.useState("Init");
+
+//   useSubscription('/topic/message', function (msg) {
+//     if (msg.body) {
+//       const jsonBody = JSON.parse(msg.body);
+//       if (jsonBody.name) {
+//         setMessage(jsonBody.name);
+//       }
+//     }
+//   })
+
+//   return (
+//     <div
+//       style={{
+//         width: "100%",
+//         color: "black",
+//         fontSize: "90px",
+//         textAlign: "center",
+//         paddingTop: "60px",
+//       }}
+//     >
+//       {message}
+//     </div>
+//   );
+// }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./css/style.css";
+// import "./css/style.css";
 import { Provider } from "react-redux";
 import { appStore } from "./Redux/AppState";
 import { HashRouter } from "react-router-dom";
